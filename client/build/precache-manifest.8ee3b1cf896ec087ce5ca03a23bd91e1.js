08eafe2aeffc083f6aa527b5d4a9b700fbf14fdd
Diem Ly <Diemrosely@gmail.com>
2020-08-28 08:39:46 -0600

    run build

---
+++ client/build/precache-manifest.8ee3b1cf896ec087ce5ca03a23bd91e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721256e054dfe2b46a62144088787ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a23126048bc8fa0a74d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "253008c6cd425c0726bf",
+    "url": "/static/css/main.d1e9cd44.chunk.css"
+  },
+  {
+    "revision": "3a23126048bc8fa0a74d",
+    "url": "/static/js/2.1f7a27a8.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.1f7a27a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253008c6cd425c0726bf",
+    "url": "/static/js/main.f248f896.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "1f87e58f5fa40e11999dfee9b9ad21c5",
+    "url": "/static/media/header.1f87e58f.jpg"
+  }
+]);
