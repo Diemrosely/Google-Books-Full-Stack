2157e35aa05a9bdf254e2a9a5a6b664c96a63dc2
Diem Ly <Diemrosely@gmail.com>
2020-08-28 13:43:54 -0600

    heroku

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 const app = express();
 
+
 //allow both json and urlencoded to be sent to the server
 //handle input from json
 app.use(bodyParser.json());
 //handle input from post
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//use route file as routes
-app.use('/', routes);
 //allow access to the static path client/build
 app.use(express.static(path.join(__dirname, 'client/build')))
 
+//use route file as routes
+app.use('/', routes);
+app.use("*", function(req, res){
+    res.sendFile("./client/build/index.html");
+});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/");
 //listening port
 app.listen(port, function(){
 	console.log(`Express started on http://localhost:${port} - press Ctrl-C to exit`);
